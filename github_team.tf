933aa92a96f2fec31e60eaaa1db3597709218571
Glenn Bech <glenn.bech@gmail.com>
2018-09-17 06:58:41 +0200

    fix

--- github_team.tf
+++ github_team.tf
@@ -3,13 +3,13 @@ resource "github_team" "theclass" {
   description = "The students and teacher of the class"
 }
 
-resource "github_team_membership" "members" {
+resource "github_team_membership" "glennbech" {
   team_id  = "${github_team.theclass.id}"
   username = "glennbech"
   role     = "member"
 }
 
-resource "github_team_membership" "members" {
+resource "github_team_membership" "larstobi" {
   team_id  = "${github_team.theclass.id}"
   username = "larstobi"
   role     = "member"
